a437e5878a79b0c2143341a1a3732dee6d7967e7
Nathan Guerrero <hihacks@gmail.com>
2015-05-18 09:50:43 -0700

    Create main game file
    Main game file created. Made file executable for many systems(including Raspberry Pi). Added extra info. Added multiple required libraries

---
+++ pong.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# A remake of the classic game, Pong, designed for Raspberry Pis with special controllers.
+# Copyright 2015, Nathan Guerrero & Giovanni Galvan, under GPLv2.
+
+import pygame
+import sys
+import random
