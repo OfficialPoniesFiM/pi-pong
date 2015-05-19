cdfc1e6a0a6099bc58569d3672827a05eb0fbbe2
Nathan Guerrero <hihacks@gmail.com>
2015-05-19 14:21:52 -0700

    pong.py transfer 1
    used geany, text transfered

--- pong.py
+++ pong.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
-# A remake of the classic game, Pong, designed for Raspberry Pis with special controllers.
-# Copyright 2015, Nathan Guerrero & Giovanni Galvan, under GPLv2.
 
+# -*- coding: utf-8 -*-
+#
+#  pong.py
+#  
+#  Copyright 2015, Nathan Guerrero and Giovanni Galvan @ Chopper Studios
+#  PiPong - A recreation of the classic game designed for the Raspberry Pi with special controllers.  
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+# We need to import things for them to work.
 import pygame
 import sys
 import random
+import RPi.GPIO as GPIO
+import time
+
+if __name__ == '__main__':
+	main()
+
