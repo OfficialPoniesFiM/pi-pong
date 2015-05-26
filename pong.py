5540b70c4d62f753539a5190d05bedc13752b528
Nathan Guerrero <hihacks@gmail.com>
2015-05-26 11:56:53 -0700

    key

--- pong.py
+++ pong.py
@@ -28,7 +28,6 @@
 import pygame
 import sys
 import random
-import RPi.GPIO as GPIO
 import time
 
 currentlyRunning = True
@@ -40,6 +39,9 @@ pixelSizeX = 20
 pixelSizeY = 16
 pixelSize = [pixelSizeX, pixelSizeY]
 BACKGROUNDCOLOR = (104, 255, 142)
+WHITE = (255, 255, 255)
+pongStart = False
+pongPlay = False
 
 def pixelRasterize(pixelMultiplier, xy):
 	if xy == True:
@@ -53,70 +55,70 @@ def characterRender(character, color, carX, carY):
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, True)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, True)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(5, True), pixelRasterize(1, False)])
-	else if character == "a":
+	elif character == "a":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(.5, False), pixelRasterize(1, True), pixelRasterize(6, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1.5, False), pixelRasterize(1, True), pixelRasterize(4, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, True), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(2, True), pixelRasterize(5, True), pixelRasterize(1, False)])
-	else if character == "B":
-		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False])
+	elif character == "B":
+		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(3, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(3, False), pixelRasterize(3, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(3, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False) - pixelRasterize(1.5, False), pixelRasterize(1, True), pixelRasterize(2, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1.5, False), pixelRasterize(1, True), pixelRasterize(2, False)])
-	else if character == "b":
+	elif character == "b":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(1, True), pixelRasterize(3, False)])
-	else if character == "C":
+	elif character == "C":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
-	else if character == "c":
+	elif character == "c":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(1, True), pixelRasterize(5, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
-	else if character == "D":
+	elif character == "D":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(1, True), pixelRasterize(carY, False) - pixelRasterize(3, False), pixelRasterize(3, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(1, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(3, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(3, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(1, True), pixelRasterize(carY, False) - pixelRasterize(2, False), pixelRasterize(1, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(1, True), pixelRasterize(carY, False) + pixelRasterize(2, False), pixelRasterize(1, True), pixelRasterize(1, False)])
-	else if character == "d":
+	elif character == "d":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(5, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(1, True), pixelRasterize(3, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
-	else if character == "E":
+	elif character == "E":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
-	else if character == "e":
+	elif character == "e":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(3, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(1, True), pixelRasterize(5, False)])
-	else if character == "F":
+	elif character == "F":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
-	else if character == "f":
+	elif character == "f":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(1, True), pixelRasterize(5, False)])
-	else if character == "G":
+	elif character == "G":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) - pixelRasterize(2, True), pixelRasterize(carY, False), pixelRasterize(1, True), pixelRasterize(7, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(1, True), pixelRasterize(carY, False), pixelRasterize(3, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True) + pixelRasterize(2, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(1, True), pixelRasterize(3, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
-	else if character == "g":
+	elif character == "g":
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) - pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(1, False), pixelRasterize(5, True), pixelRasterize(1, False)])
 		mainScreen.fill(color, rect=[pixelRasterize(carX, True), pixelRasterize(carY, False) + pixelRasterize(3, False), pixelRasterize(5, True), pixelRasterize(1, False)])
@@ -128,7 +130,87 @@ while currentlyRunning:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			currentlyRunning = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == K_RETURN:
+				if pongPlay == False:
+					pongStart = True
+			if event.key == K_W:
+				pMoveOne = 1
+			elif event.key == K_S:
+				pMoveOne = 2
+			else:
+				pMoveOne = 3
+			if event.key == K_UP:
+				pMoveTwo = 1
+			elif event.key == K_DOWN:
+				pMoveTwo = 2
+			else:
+				pMoveTwo = 3
 	mainScreen.fill(BACKGROUNDCOLOR)
+	if pongStart == True:
+		ponger = random.randint(1, 64)
+		pOne = 32
+		pTwo = 32
+		pOneP = 0
+		pTwoP = 0
+		pBall = 32
+		pProgress = 0
+		pMove = False
+		pongStart = False
+		pongPlay = True
+	if pongPlay == True:
+		if pongMoveOne == 1:
+			pOne -= 1
+		elif pongMoveOne == 2:
+			pOne += 1
+		if pongMoveTwo == 1:
+			pTwo -= 1
+		elif pongMoveTwo == 2:
+			pTwo += 1
+		if pMove == False:
+			pProgress += 1
+			if pBall != ponger:
+				if pBall < ponger:
+					pBall += 1
+				else:
+					pBall -= 1
+		elif pMove == True:
+			pProgress -= 1
+			if pBall != ponger:
+				if pBall < ponger:
+					pBall += 1
+				else:
+					pBall -= 1
+		if pProgress == -28 and pBall in range(pOne - 2, pOne + 2):
+			ponger = random.randint(1, 64)
+			if pMove == False:
+				pMove = True
+			elif pMove == True:
+				pMove == False
+		elif pProgress == 28 and pBall in range(pTwo -2, pTwo + 2):
+			ponger = random.randint(1, 64)
+			if pMove == False:
+				pMove = True
+			elif pMove == True:
+				pMove == False
+		if pProgress == -32:
+			pProgress == 0
+			pTwoP += 1
+		elif pProgress ==  32:
+			pProgress == 0
+			pOneP += 1
+		if pOneP == 5:
+			characterRender("A", (255,255,255), 16, 32)
+			time.sleep(10)
+			pongPlay = False
+		if pTwoP == 5:
+			characterRender("A", (255,255,255), 48, 32)
+			time.sleep(10)
+			pongPlay = False
+		
+		mainScreen.fill(WHITE, rect=[pixelRasterize(3, True), pixelRasterize(pOne, False), pixelRasterize(1, True), pixelRasterize(5, True)])
+		mainScreen.fill(WHITE, rect=[pixelRasterize(61, True), pixelRasterize(pTwo, False), pixelRasterize(1, True), pixelRasterize(5, True)])
+		mainScreen.fill(WHITE, rect=[pixelRasterize(pProgress + 32, True), pixelRasterize(pBall, False), pixelRasterize(1, True), pixelRasterize(1, False)])
 	pygame.display.update()
 	time.sleep(1 / 60)
 pygame.quit()
